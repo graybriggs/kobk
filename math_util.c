73c988889cee6dabc759ca454428822513b71e5e
Graham Briggs <grahambriggs@gmail.com>
2023-08-21 20:49:18 +0900

    important matrix stuff

--- math_util.c
+++ math_util.c
@@ -8,19 +8,167 @@
 #include <stdint.h>
 
 
-void mat4_identity(mat4* mat) {
+void mat4_set_identity(mat4* mat) {
 	memset(mat->m, 0, sizeof(mat->m));
-	mat->m[0] = 1.0f;
-	mat->m[5] = 1.0f;
-	mat->m[10] = 1.0f;
-	mat->m[15] = 1.0f;
+	mat->m[0] = mat->m[5] = mat->m[10] = mat->m[15] = 1.0f;
 }
 
-void mat4_translation(mat4* mat, vec3 a) {
+
+vec3 vec3_transform(vec3 a, mat4* mat) {
+	float w = mat->m[3] * a.x + mat->m[7] * a.y + mat->m[11] * a.z + mat->m[15];
+	if (w == 0) {
+		w = 1;
+	}
+	return VEC3_INIT(
+		(mat->m[0] * a.x + mat->m[4] * a.y + mat->m[8] * a.z + mat->m[12]) / w,
+		(mat->m[1] * a.x + mat->m[5] * a.y + mat->m[9] * a.z + mat->m[13]) / w,
+		(mat->m[2] * a.x + mat->m[6] * a.y + mat->m[10] * a.z + mat->m[14]) / w
+	);
+}
+
+
+void mat4_translate(mat4* mat, vec3 trans) {
+	mat->m[12] = mat->m[0] * trans.x + mat->m[4] * trans.y + mat->m[8] * trans.z + mat->m[12];
+	mat->m[13] = mat->m[1] * trans.x + mat->m[5] * trans.y + mat->m[9] * trans.z + mat->m[13];
+	mat->m[14] = mat->m[2] * trans.x + mat->m[6] * trans.y + mat->m[10] * trans.z + mat->m[14];
+	mat->m[15] = mat->m[3] * trans.x + mat->m[7] * trans.y + mat->m[11] * trans.z + mat->m[15];
+}
+
+
+void mat4_scale(mat4* mat, float x, float y, float z) {
+
+}
+
+void mat4_rotate(mat4* mat, float angle, vec3 u) {
+
+		float c = cos(angle);
+		float s = sin(angle);
+		float t = 1.0f - c;
+
+		float x = u.x;
+		float y = u.y;
+		float z = u.z;
+
+		// Normalize the axis vector
+		float length = sqrt(x * x + y * y + z * z);
+		if (length > 0.0f) {
+			x /= length;
+			y /= length;
+			z /= length;
+		}
+
+		float tx = t * x;
+		float ty = t * y;
+		float tz = t * z;
+
+		float sx = s * x;
+		float sy = s * y;
+		float sz = s * z;
+
+		// Rotation matrix
+		//mat4 rotation_matrix;
+		mat4 rotation_matrix = MAT4_INIT(
+			tx * x + c, tx * y - sz, tx * z + sy, 0.0f,
+			tx * y + sz, ty * y + c, ty * z - sx, 0.0f,
+			tx * z - sy, ty * z + sx, tz * z + c, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f
+		);
+
+		// Apply rotation to the input matrix
+		mat4 result_matrix;
+		mat4_set_identity(&result_matrix);
+		
+		for (int i = 0; i < 16; ++i) {
+			result_matrix.m[i] = 0.0f;
+			for (int j = 0; j < 4; ++j) {
+				result_matrix.m[i] += rotation_matrix.m[i % 4 + j * 4] * mat->m[j * 4 + i / 4];
+			}
+		}
+
+		// Copy the result back to the input matrix
+		for (int i = 0; i < 16; ++i) {
+			mat->m[i] = result_matrix.m[i];
+		}
+
+	/*
+	if (angle == 0)
+		return;
+
+	mat4 rotate_x;
+	mat4 rotate_y;
+	mat4 rotate_z;
+	mat4_set_identity(&rotate_x);
+	mat4_set_identity(&rotate_y);
+	mat4_set_identity(&rotate_z);
 	
+
+	rotate_x[1][1] = cos(angle_x);
+	rotate_x[1][2] = sin(angle_x);
+	rotate_x[2][1] = -sin(angle_x);
+	rotate_x[2][2] = cos(angle_x);
+
+	Mat_Identity_4x4(rotate_y);
+
+	rotate_y[0][0] = cos(angle_y);
+	rotate_y[0][2] = -sin(angle_y);
+	rotate_y[2][0] = sin(angle_y);
+	rotate_y[2][2] = cos(angle_y);
+
+	Mat_Identity_4x4(rotate_z);
+
+	rotate_z[0][0] = cos(angle_z);
+	rotate_z[0][1] = sin(angle_z);
+	rotate_z[1][0] = -sin(angle_z);
+	rotate_z[1][1] = cos(angle_z);
+
+	mat4 temp;
+	mat4_set_identity(&temp);
+
+	mat4_mul(&temp, &rotate_x, &rotate_y);
+	mat4_mul(&mat, &rotate_z, &temp);
+	*/
 }
 
-// void mat4_mult(mat4* res, mat4* a, mat4* b);
+void mat4_mul(mat4* res, mat4* a, mat4* b) {
+	res->m[0] = b->m[0] * a->m[0] + b->m[1] * a->m[4] + b->m[2] * a->m[8] + b->m[3] * a->m[12];
+	res->m[1] = b->m[0] * a->m[1] + b->m[1] * a->m[5] + b->m[2] * a->m[9] + b->m[3] * a->m[13];
+	res->m[2] = b->m[0] * a->m[2] + b->m[1] * a->m[6] + b->m[2] * a->m[10] + b->m[3] * a->m[14];
+	res->m[3] = b->m[0] * a->m[3] + b->m[1] * a->m[7] + b->m[2] * a->m[11] + b->m[3] * a->m[15];
+	res->m[4] = b->m[4] * a->m[0] + b->m[5] * a->m[4] + b->m[6] * a->m[8] + b->m[7] * a->m[12];
+	res->m[5] = b->m[4] * a->m[1] + b->m[5] * a->m[5] + b->m[6] * a->m[9] + b->m[7] * a->m[13];
+	res->m[6] = b->m[4] * a->m[2] + b->m[5] * a->m[6] + b->m[6] * a->m[10] + b->m[7] * a->m[14];
+	res->m[7] = b->m[4] * a->m[3] + b->m[5] * a->m[7] + b->m[6] * a->m[11] + b->m[7] * a->m[15];
+	res->m[8] = b->m[8] * a->m[0] + b->m[9] * a->m[4] + b->m[10] * a->m[8] + b->m[11] * a->m[12];
+	res->m[9] = b->m[8] * a->m[1] + b->m[9] * a->m[5] + b->m[10] * a->m[9] + b->m[11] * a->m[13];
+	res->m[10] = b->m[8] * a->m[2] + b->m[9] * a->m[6] + b->m[10] * a->m[10] + b->m[11] * a->m[14];
+	res->m[11] = b->m[8] * a->m[3] + b->m[9] * a->m[7] + b->m[10] * a->m[11] + b->m[11] * a->m[15];
+	res->m[12] = b->m[12] * a->m[0] + b->m[13] * a->m[4] + b->m[14] * a->m[8] + b->m[15] * a->m[12];
+	res->m[13] = b->m[12] * a->m[1] + b->m[13] * a->m[5] + b->m[14] * a->m[9] + b->m[15] * a->m[13];
+	res->m[14] = b->m[12] * a->m[2] + b->m[13] * a->m[6] + b->m[14] * a->m[10] + b->m[15] * a->m[14];
+	res->m[15] = b->m[12] * a->m[3] + b->m[13] * a->m[7] + b->m[14] * a->m[11] + b->m[15] * a->m[15];
+}
+
+
+void look_at(vec3 position, vec3 target, vec3 up) {
+
+}
+
+
+void mat4_perspective(mat4* mat) {
+	float frustum_scale = 1.0f;
+	float z_near = 0.5f;
+	float z_far = 3.0f;
+	
+	mat->m[0] = frustum_scale;
+	mat->m[5] = frustum_scale;
+	mat->m[10] = (z_far + z_near) / (z_near - z_far);
+	mat->m[11] = (2 * z_far * z_near) / (z_near - z_far);
+	mat->m[14] = -1.0f;
+}
+
+void mat4_ortho(mat4* mat) {
+
+}
 
 ///////
 
@@ -30,4 +178,14 @@ void vec2_print(const vec2 a) {
 
 void vec3_print(const vec3 a) {
 	printf("%f %f %f\n", a.x, a.y, a.z);
+}
+
+void mat4_print(const mat4* a) {
+	int count = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%.2f ", a->m[count++]);
+		}
+		printf("\n");
+	}
 }
--- math_util.h
+++ math_util.h
@@ -26,28 +26,31 @@ typedef struct {
 #define VEC2_INIT(X, Y) ((vec2){.x = X, .y = Y})
 #define VEC3_INIT(X, Y, Z) ((vec3){.x = X, .y = Y, .z = Z})
 
+#define MAT4_IDENT(m) \
+	(mat4){.m = { \
+		1.0f, 0.0f, 0.0f, 0.0f, \
+		0.0f, 1.0f, 0.0f, 0.0f, \
+		0.0f, 0.0f, 1.0f, 0.0f, \
+		0.0f, 0.0f, 0.0f, 1.0f  \
+	}}
+
+#define MAT4_INIT(m0,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15) \
+	(mat4){.m = { \
+		m0,   m1,  m2,  m3, \
+		m4,   m5,  m6,  m7, \
+		m8,   m9, m10, m11, \
+		m12, m13, m14, m15  \
+	}}
 
 
-static inline float vec3_length(vec3 a) {
-	return (float)sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+static inline float vec2_length(vec2 a) {
+	return (float)sqrt(a.x * a.x + a.y * a.y);
 }
 
-static inline float vec3_mag(vec2 a) {
-
-static inline float vec3_mag(vec3 a) {
-
+static inline float vec2_mag(vec2 a) {
+	return 0.0f;
 }
 
-static inline vec3 vec3_normalize(vec3 a) {
-
-}
-
-static inline vec2 vec2_invert(vec2 a) {
-	return VEC2_INIT(
-		-a.x,
-		-a.y
-	);
-}
 
 // vec2
 
@@ -61,7 +64,7 @@ static inline vec2 vec2_invert(vec2 a) {
 static inline vec2 vec2_add(vec2 a, vec2 b) {
 	return VEC2_INIT(
 		a.x + b.x,
-		a.y + b.y,
+		a.y + b.y
 	);
 }
 
@@ -83,12 +86,16 @@ static inline vec2 vec2_mul(vec2 a, vec2 b) {
 static inline vec2 vec_mulf(vec2 a, float f) {
 	return VEC2_INIT(
 		a.x * f,
-		a.y* f;
+		a.y* f
 	);
 }
 
 
 // vec3
+
+static inline float vec3_length(vec3 a) {
+	return (float)sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+}
 
 static inline vec3 vec3_invert(vec3 a) {
 	return VEC3_INIT(
@@ -125,7 +132,7 @@ static inline vec3 vec3_mul(vec3 a, vec3 b) {
 //static inline vec3 vec3_div(vec3 a, vec3 b); // ??
 
 
-static inline vec3 vec_mulf(vec3 a, float f) {
+static inline vec3 vec3_mulf(vec3 a, float f) {
 	return VEC3_INIT(
 		a.x * f,
 		a.y * f,
@@ -152,11 +159,19 @@ static inline float vec3_dot(vec3 a, vec3 b) {
 
 /////////////////
 
-void mat4_identity(mat4* mat);
-void mat4_translation(mat4* mat);
+void mat4_set_identity(mat4* mat);
+void mat4_translate(mat4* mat, vec3 trans);
+void mat4_scale(mat4* mat, float x, float y, float z);
+void mat4_rotate(mat4* mat, float angle, vec3 u);
+void mat4_mul(mat4* res, mat4* a, mat4* b);
 
+void look_at(vec3 position, vec3 target, vec3 up);
+
+void mat4_perspective(mat4* mat);
+void mat4_ortho(mat4* mat);
 
 ///////////
 
 void vec2_print(const vec2 a);
 void vec3_print(const vec3 a);
+void mat4_print(const mat4* a);
