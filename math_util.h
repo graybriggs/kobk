0f8f25246fc808f66fb68011411d32de3b95888b
Graham Briggs <grahambriggs@gmail.com>
2023-12-26 13:17:27 +0900

    static inline over macro

--- math_util.h
+++ math_util.h
@@ -1,11 +1,12 @@
 #pragma once
 
-#include <math.h>
-#include <string.h>
-#include <stdint.h>
 
 #define PI 3.14159265
 #define DEG2RAD(A) (A) * (PI / 180)
+
+static inline float deg2rad(float angle) {
+	return angle * (PI / 180);
+}
 
 typedef struct {
 	float x;
@@ -43,130 +44,37 @@ typedef struct {
 	}}
 
 // |v|
-static inline float vec2_mag(vec2 a) {
-	return (float)sqrt(a.x * a.x + a.y * a.y);
-}
+float vec2_mag(vec2 a);
 
 
 // vec2
 
-static inline vec2 vec2_invert(vec2 a) {
-	return VEC2_INIT(
-		-a.x,
-		-a.y
-	);
-}
-
-static inline vec2 vec2_add(vec2 a, vec2 b) {
-	return VEC2_INIT(
-		a.x + b.x,
-		a.y + b.y
-	);
-}
-
-static inline vec2 vec2_sub(vec2 a, vec2 b) {
-	return VEC2_INIT(
-		a.x - b.x,
-		a.y - b.y
-	);
-}
-
-static inline vec2 vec2_mul(vec2 a, vec2 b) {
-	return VEC2_INIT(
-		a.x * b.x,
-		a.y * b.y
-	);
-}
-
-
-static inline vec2 vec_mulf(vec2 a, float f) {
-	return VEC2_INIT(
-		a.x * f,
-		a.y* f
-	);
-}
-
+vec2 vec2_invert(vec2 a);
+vec2 vec2_add(vec2 a, vec2 b);
+vec2 vec2_sub(vec2 a, vec2 b);
+vec2 vec2_mul(vec2 a, vec2 b);
+vec2 vec2_mulf(vec2 a, float f);
 
 // vec3
 
 // |u|
-static inline float vec3_mag(vec3 a) {
-	return (float)sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
-}
-
-
-static inline vec3 vec3_invert(vec3 a) {
-	return VEC3_INIT(
-		-a.x,
-		-a.y,
-		-a.z
-	);
-}
-
-static inline vec3 vec3_add(vec3 a, vec3 b) {
-	return VEC3_INIT(
-		a.x + b.x,
-		a.y + b.y,
-		a.z + b.z
-	);
-}
-
-static inline vec3 vec3_sub(vec3 a, vec3 b) {
-	return VEC3_INIT(
-		a.x - b.x,
-		a.y - b.y,
-		a.z - b.z
-	);
-}
-
-static inline vec3 vec3_mul(vec3 a, vec3 b) {
-	return VEC3_INIT(
-		a.x * b.x,
-		a.y * b.y,
-		a.z * b.z
-	);
-}
+float vec3_mag(vec3 a);
+vec3 vec3_invert(vec3 a);
+vec3 vec3_add(vec3 a, vec3 b);
+vec3 vec3_sub(vec3 a, vec3 b);
+vec3 vec3_mul(vec3 a, vec3 b);
 
 //static inline vec3 vec3_div(vec3 a, vec3 b); // ??
 
 
-static inline vec3 vec3_mulf(vec3 a, float f) {
-	return VEC3_INIT(
-		a.x * f,
-		a.y * f,
-		a.z * f
-	);
-}
+vec3 vec3_mulf(vec3 a, float f);
 
 /////
 
-static inline float vec3_dot(vec3 a, vec3 b) {
-	return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-static inline vec3 vec3_cross(vec3 a, vec3 b) {
-	return VEC3_INIT(
-		a.y * b.z - a.z * b.y,
-		a.z * b.x - a.x * b.z,
-		a.x * b.y - a.y * b.x
-	);
-}
-
-
-static inline vec2 vec2_from_2d_points(vec3 init, vec3 term) {
-	return VEC2_INIT(
-		term.x - init.x,
-		term.y - init.y
-	);
-}
-
-static inline vec3 vec3_from_3d_points(vec3 init, vec3 term) {
-	return VEC3_INIT(
-		term.x - init.x,
-		term.y - init.y,
-		term.z - init.z
-	);
-}
+float vec3_dot(vec3 a, vec3 b);
+vec3 vec3_cross(vec3 a, vec3 b);
+vec2 vec2_from_2d_points(vec3 init, vec3 term);
+vec3 vec3_from_3d_points(vec3 init, vec3 term);
 
 
 /////////////////
